53dd41de87ca6a00837bbd7f30fc33cf64c0ca6b
jeonDev <jjhwhdgus0727@gmail.com>
2024-01-03 21:25:48 +0900

    Crypt Util Setting

---
+++ src/main/java/com/commerce/core/common/utils/EncryptUtils.java
@@ -0,0 +1,22 @@
+package com.commerce.core.common.utils;
+
+public class EncryptUtils {
+
+    /**
+     * Encrypt
+     * @param value
+     * @return
+     */
+    public static String encrypt(String value) {
+        return value;
+    }
+
+    /**
+     * Decrypt
+     * @param value
+     * @return
+     */
+    public static String decrypt(String value) {
+        return value;
+    }
+}
