57300d903c603687d36007e1710dcaa6d19dbd6e
jeonDev <jjhwhdgus0727@gmail.com>
2024-01-16 23:13:23 +0900

    MongoDB Setting

--- src/main/resources/sql/console.sql
+++ src/main/resources/sql/console.sql
@@ -8,3 +8,14 @@ grant all privileges on order_system.* to 'order_system'@'%';
 FLUSH PRIVILEGES;
 /* 여기까지 */
 
+
+/*
+docker pull mongo
+docker run --name mongodb -v ~/data:/data/db -d -p 27017:27017 mongo
+docker exec -it mongodb /bin/bash
+mongosh
+
+use commerce;
+db.createUser({user:"commerce", pwd:"commerce", roles:["readWrite"]});
+db.createCollection("PRODUCT_VIEW"); // Collection생성
+ */
