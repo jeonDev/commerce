c9402003aee4d2c793810ba2d99dd579df6b8da4
jeonDev <jjhwhdgus0727@gmail.com>
2024-02-04 22:25:39 +0900

    Point DB Lock Add

--- src/main/java/com/commerce/core/point/repository/PointRepository.java
+++ src/main/java/com/commerce/core/point/repository/PointRepository.java
@@ -2,11 +2,15 @@ package com.commerce.core.point.repository;
 
 import com.commerce.core.member.entity.Member;
 import com.commerce.core.point.entity.Point;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 
 import java.util.Optional;
 
 public interface PointRepository extends JpaRepository<Point, Long> {
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
     Optional<Point> findByMember(Member member);
     Optional<Point> findByMember_MemberSeq(Long memberSeq);
 }
--- src/test/java/com/commerce/core/point/PointTest.java
+++ src/test/java/com/commerce/core/point/PointTest.java
@@ -4,17 +4,23 @@ import com.commerce.core.common.exception.CommerceException;
 import com.commerce.core.point.entity.Point;
 import com.commerce.core.point.service.PointService;
 import com.commerce.core.point.vo.PointDto;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@Slf4j
 @SpringBootTest
 public class PointTest {
-
+    final int COUPON_THREAD_COUNT = 10;
     @Autowired
     private PointService pointService;
 
@@ -24,18 +30,21 @@ public class PointTest {
         PointDto pointDto = new PointDto();
         pointDto.setMemberSeq(1L);
         pointDto.setPoint(10000L);
+        Point originPoint = pointService.selectPoint(pointDto.getMemberSeq()).get();
         pointService.charge(pointDto);
 
         Point point = pointService.selectPoint(pointDto.getMemberSeq()).get();
-        assertThat(point.getPoint()).isEqualTo(pointDto.getPoint());
+        assertThat(point.getPoint()).isEqualTo(pointDto.getPoint() + originPoint.getPoint());
     }
 
     @Test
     @Order(2)
     void pointPayment_fail() {
+
         PointDto pointDto = new PointDto();
         pointDto.setMemberSeq(1L);
-        pointDto.setPoint(20000L);
+        Point originPoint = pointService.selectPoint(pointDto.getMemberSeq()).get();
+        pointDto.setPoint(originPoint.getPoint() + 1L);
         assertThrows(CommerceException.class, () -> {
             pointService.withdraw(pointDto);
         });
@@ -48,8 +57,34 @@ public class PointTest {
         PointDto pointDto = new PointDto();
         pointDto.setMemberSeq(1L);
         pointDto.setPoint(10000L);
+        Point originPoint = pointService.selectPoint(pointDto.getMemberSeq()).get();
+
         pointService.withdraw(pointDto);
         Point point = pointService.selectPoint(pointDto.getMemberSeq()).get();
-        assertThat(point.getPoint()).isEqualTo(0L);
+        assertThat(point.getPoint()).isEqualTo(originPoint.getPoint() - pointDto.getPoint());
+    }
+
+    @Test
+    void point_lock_test() throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(COUPON_THREAD_COUNT);
+        CountDownLatch latch = new CountDownLatch(COUPON_THREAD_COUNT);
+
+        for(int i = 0; i < COUPON_THREAD_COUNT; i++) {
+            PointDto pointDto = new PointDto();
+            pointDto.setMemberSeq(2L);
+            pointDto.setPoint(1000L);
+            int cnt = i;
+            executorService.execute(() -> {
+                try {
+                    if(cnt % 2 == 0) pointService.charge(pointDto);
+                    else pointService.withdraw(pointDto);
+                } catch (Exception e) {
+                    log.error("error : {}", e);
+                }
+                latch.countDown();
+            });
+        }
+
+        latch.await();
     }
 }
