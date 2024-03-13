ce5670d99d5fb85c4691c6e6e2f5c4dab4d5968d
jeonDev <jjhwhdgus0727@gmail.com>
2024-03-13 23:20:34 +0900

    Cors Config

--- src/main/java/com/commerce/core/common/config/WebMvcConfig.java
+++ src/main/java/com/commerce/core/common/config/WebMvcConfig.java
@@ -4,10 +4,17 @@ import com.commerce.core.common.filter.LoggingFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*");
+    }
 
     @Bean
     public FilterRegistrationBean<LoggingFilter> loggingFilter() {
