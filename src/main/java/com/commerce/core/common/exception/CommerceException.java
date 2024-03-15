68dc87cf19ad32a05f5714aaa7cacec07fa6f228
jeonDev <79624332+jeonDev@users.noreply.github.com>
2024-03-15 14:05:12 +0900

    Custom Exception message Edit

--- src/main/java/com/commerce/core/common/exception/CommerceException.java
+++ src/main/java/com/commerce/core/common/exception/CommerceException.java
@@ -6,14 +6,16 @@ import lombok.Getter;
 public class CommerceException extends RuntimeException {
 
     private final String code;
+    private final String message;
 
     public CommerceException(ExceptionStatus exceptionStatus) {
-        super(exceptionStatus.getMessage());
         this.code = exceptionStatus.getCode();
+        this.message = exceptionStatus.getMessage();
     }
 
     public CommerceException(Exception e) {
         super(e);
         this.code = "error";
+        this.message = e.getMessage();
     }
 }
