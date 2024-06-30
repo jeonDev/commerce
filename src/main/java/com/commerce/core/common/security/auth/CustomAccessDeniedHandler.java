719de8bb7a5e251f6a1647abe2f96dd87cae1d73
jeonDev <jjhwhdgus0727@gmail.com>
2024-06-30 14:55:40 +0900

    feat ( security ) : Auth Http Status Response Add

--- src/main/java/com/commerce/core/common/security/auth/CustomAccessDeniedHandler.java
+++ src/main/java/com/commerce/core/common/security/auth/CustomAccessDeniedHandler.java
@@ -4,6 +4,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
@@ -15,10 +17,15 @@ import java.io.IOException;
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
     /**
-     * 401 UNAUTHORIZED
+     * 403 FORBIDDEN
      * */
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
         log.error("Access Denied Handler : {}", request.getRequestURI());
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setCharacterEncoding("UTF-8");
+//        response.getWriter().write();
     }
 }
--- src/main/java/com/commerce/core/common/security/auth/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/commerce/core/common/security/auth/CustomAuthenticationEntryPoint.java
@@ -4,6 +4,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -15,12 +17,16 @@ import java.io.IOException;
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     /**
-     * 403 FORBIDDEN
+     * 401 UNAUTHORIZED
      */
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
         log.error("Authentication Entry Point : {}", request.getRequestURI());
 
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.setCharacterEncoding("UTF-8");
+//        response.getWriter().write();
     }
 }
 
