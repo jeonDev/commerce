f204dc85c4797995eb818a343d3fc406f9daba31
jeonDev <jjhwhdgus0727@gmail.com>
2025-02-17 21:58:22 +0900

    chore ( redisson ) : Redisson 사용부분 제거

--- src/main/java/com/commerce/core/common/config/redis/RedissonLockAspect.java
+++ src/main/java/com/commerce/core/common/config/redis/RedissonLockAspect.java
@@ -7,8 +7,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.redisson.api.RLock;
-import org.redisson.api.RedissonClient;
+//import org.redisson.api.RLock;
+//import org.redisson.api.RedissonClient;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -26,35 +26,35 @@ public class RedissonLockAspect {
 
     private static final int WAIT_TIME_SECOND = 5;
 
-    private final RedissonClient redissonClient;
-
-    public RedissonLockAspect(RedissonClient redissonClient) {
-        this.redissonClient = redissonClient;
-    }
-
-    // com.commerce.core.common.redis.RedissonLockTarget
-//    @Around("@annotation(redisTarget)")
-    public Object redisLockAspect(ProceedingJoinPoint joinPoint, RedissonLockTarget redisTarget) throws Throwable {
-        log.info("Redisson Proxy 호출!!");
-
-        final String LOCK_TARGET = redisTarget.value().getKey();
-        final int LEASE_TIME = redisTarget.leaseTime();
-
-        Object result = null;
-
-        RLock lock = redissonClient.getLock(LOCK_TARGET);
-        try {
-
-            if(!lock.tryLock(WAIT_TIME_SECOND, LEASE_TIME, TimeUnit.SECONDS)) {
-                log.error("Redisson Lock 획득 실패");
-                throw new CommerceException(ExceptionStatus.LOCK_OCCUPIED_ERROR);
-            }
-
-            result = joinPoint.proceed();
-
-        } finally {
-            lock.unlock();
-        }
-        return result;
-    }
+//    private final RedissonClient redissonClient;
+//
+//    public RedissonLockAspect(RedissonClient redissonClient) {
+//        this.redissonClient = redissonClient;
+//    }
+//
+//    // com.commerce.core.common.redis.RedissonLockTarget
+////    @Around("@annotation(redisTarget)")
+//    public Object redisLockAspect(ProceedingJoinPoint joinPoint, RedissonLockTarget redisTarget) throws Throwable {
+//        log.info("Redisson Proxy 호출!!");
+//
+//        final String LOCK_TARGET = redisTarget.value().getKey();
+//        final int LEASE_TIME = redisTarget.leaseTime();
+//
+//        Object result = null;
+//
+//        RLock lock = redissonClient.getLock(LOCK_TARGET);
+//        try {
+//
+//            if(!lock.tryLock(WAIT_TIME_SECOND, LEASE_TIME, TimeUnit.SECONDS)) {
+//                log.error("Redisson Lock 획득 실패");
+//                throw new CommerceException(ExceptionStatus.LOCK_OCCUPIED_ERROR);
+//            }
+//
+//            result = joinPoint.proceed();
+//
+//        } finally {
+//            lock.unlock();
+//        }
+//        return result;
+//    }
 }
